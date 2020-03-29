4f3ee56c2ce086f258b3357aa6f821e9119dfa9c
dalt4 <45166154+dalt4@users.noreply.github.com>
2020-03-29 17:20:34 +0300

    end for now 3)))

--- clock@timer/script.js
+++ clock@timer/script.js
@@ -9,7 +9,29 @@ var days = clock.querySelector('.days span'),
     seconds = clock.querySelector('.seconds span'),
     interval,
     timeinterval,
-    deadline = "April 02 2020 10:00:00 GMT+0300";
+    deadline = "April 02 2020 10:00:00 GMT+0300",
+    timerDays = 5,
+    timerHours = 0,
+    timerMinutes = 0,
+    timerValue = (timerDays * 86400000) + (timerHours * 3600000) + (timerMinutes * 60000);
+
+
+function getTimeRemaining(endtime, timer) {
+    var t;
+    timer !== 0 ? t = timerValue - (Date.parse(endtime) - Date.parse(new Date())) : t = Date.parse(endtime) - Date.parse(new Date());
+    var seconds = Math.floor((t / 1000) % 60);
+    var minutes = Math.floor((t / 1000 / 60) % 60);
+    var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+    var days = Math.floor(t / (1000 * 60 * 60 * 24));
+    return {
+        'total': t,
+        'days': days,
+        'hours': hours,
+        'minutes': minutes,
+        'seconds': seconds
+    };
+}
+
 
 //-----------------Clock---------------------------------//
 
@@ -52,28 +74,13 @@ var clockMode = function () {
 
 clockMode();
 
-//----------------------timer-----------------------------//
-var timerMode = function (endtime) {
+//----------------------countdown-----------------------------//
+var countdownMode = function (endtime, timer) {
 
-    function getTimeRemaining(endtime) {
-        var t = Date.parse(endtime) - Date.parse(new Date());
-        var seconds = Math.floor((t / 1000) % 60);
-        var minutes = Math.floor((t / 1000 / 60) % 60);
-        var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
-        var days = Math.floor(t / (1000 * 60 * 60 * 24));
-        return {
-            'total': t,
-            'days': days,
-            'hours': hours,
-            'minutes': minutes,
-            'seconds': seconds
-        };
-    }
-
-    function initializeTimer(endtime) {
+    function initializeTimer(endtime, timer) {
 
         function updateClock() {
-            var t = getTimeRemaining(endtime);
+            var t = getTimeRemaining(endtime, timer);
 
             t.days === 1 ? days.innerHTML = (t.days).toString() + " <sup>day</sup>" : days.innerHTML = (t.days).toString() + " <sup>days</sup>";
             hours.innerHTML = ('0' + t.hours).slice(-2);
@@ -81,7 +88,7 @@ var timerMode = function (endtime) {
             seconds.innerHTML = ('0' + t.seconds).slice(-2);
             mainTitle.innerHTML = 'CLOCK ' + ('0' + t.days).slice(-2)  + 'd ' + ('0' + t.hours).slice(-2) + ':' + ('0' + t.minutes).slice(-2) + ':' + ('0' + t.seconds).slice(-2);
 
-            if (t.total <= 0) {
+            if (t.total <= 0 || t.total === timerValue) {
                 clearInterval(timeinterval);
             }
         }
@@ -90,17 +97,23 @@ var timerMode = function (endtime) {
         timeinterval = setInterval(updateClock, 1000);
     }
 
-    initializeTimer(endtime)
+    initializeTimer(endtime, timer)
 };
 
 //-------------------------switch----------------------------------//
 
 switchTimer.addEventListener('click', function () {
     if (switchTimer.innerHTML === 'clock') {
-        switchTimer.innerHTML = 'timer';
+        switchTimer.innerHTML = 'countdown';
         days.classList.remove('dayText');
         clearInterval(interval);
-        timerMode(deadline);
+        countdownMode(deadline, 0);
+    }
+    else if  (switchTimer.innerHTML === 'countdown') {
+        switchTimer.innerHTML = 'timer';
+        days.classList.remove('dayText');
+        clearInterval(timeinterval);
+        countdownMode(deadline, timerValue);
     }
     else {
         switchTimer.innerHTML = 'clock';
